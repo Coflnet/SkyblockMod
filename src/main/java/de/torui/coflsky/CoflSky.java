d90d562d1b02ed042b7adf92ce56fe1f5a8592ac
Florian Rinke <develop@torui.de>
2021-10-02 08:48:29 +0200

    Update Version

--- src/main/java/de/torui/coflsky/CoflSky.java
+++ src/main/java/de/torui/coflsky/CoflSky.java
@@ -39,7 +39,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class CoflSky
 {
     public static final String MODID = "CoflSky";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1-Alpha";
     public static WSClientWrapper Wrapper;
     
     @EventHandler
