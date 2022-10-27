cd5ee524dfb0d1788afecb05d62aeef630d4f608
ForBai <thelars42@gmail.com>
2022-10-27 22:02:35 +0200

    final cleanup and now it should be fully done!

--- src/main/java/de/torui/coflsky/bingui/gui/BinGuiManager.java
+++ src/main/java/de/torui/coflsky/bingui/gui/BinGuiManager.java
@@ -6,7 +6,7 @@ public class BinGuiManager {
     public static BinGui currentGui = null;
 
     //this can be used to open the gui
-    public static void openFlipGui(IChatComponent message, String[] lore,String auctionId) {
+    public static void openFlipGui(IChatComponent message, String[] lore, String auctionId) {
         currentGui = new BinGui(message, lore);
         currentGui.open(auctionId);
     }
