26b5e40b9571988e97ef44d3a63c96ebc14ffe14
ForBai <thelars42@gmail.com>
2022-11-13 21:38:36 +0100

    removed things

--- src/main/java/de/torui/coflsky/bingui/helper/TestGuiOpener.java
+++ src/main/java/de/torui/coflsky/bingui/helper/TestGuiOpener.java
@@ -1,9 +1,5 @@
 package de.torui.coflsky.bingui.helper;
 
-import de.torui.coflsky.bingui.gui.BinGuiNew;
-import net.minecraft.client.Minecraft;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.IChatComponent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -11,8 +7,9 @@ public class TestGuiOpener {
     @SubscribeEvent
     public void openChestEvent(net.minecraftforge.event.entity.player.PlayerInteractEvent event) {
         //ignore this, this will be removed when the gui is finished
-        /*if (event.action.equals(PlayerInteractEvent.Action.RIGHT_CLICK_AIR)) {
-            IChatComponent message = new ChatComponentText("FLIP: §6Bat Person Helmet §2500,000 -> 750,000 (+242,500 §448%§2) §7 Med: §b750,000§7 Vol: §b20.7§r§7 sellers ah");
+        if (event.action.equals(PlayerInteractEvent.Action.RIGHT_CLICK_AIR)) {
+            /*
+            IChatComponent message = new ChatComponentText("FLIP: §6Bat Person Helmet §2500,000 -> 750,000 (+242,500 §448%§2) §7 Med: §b750,000§7 Vol: §b20.7§r§7 sellers ah aüsoijfaoßsijfá0isjf´0aisjfáoijsfá0isjf´0aijsfá9ijf");
             String[] lore = {
                     "This is a Test lore",
                     "This is a Test lore",
@@ -23,13 +20,36 @@ public class TestGuiOpener {
                     "This is a Test lore",
                     "This is a Test lore",
                     "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test loreThis is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test loreThis is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test loreThis is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test loreThis is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
+                    "This is a Test lore",
                     "This is a Test lore"
 
             };
-            Minecraft.getMinecraft().displayGuiScreen(new BinGuiNew(message, lore,"0", "minecraft:apple", null));
+            //Minecraft.getMinecraft().displayGuiScreen(new BinGuiNew(message, lore,"0", "minecraft:apple", null));
+            BinGuiManager.openNewFlipGui(message, lore, "0", "leather_leggings");
+             */
         }
-
-         */
-
     }
 }
