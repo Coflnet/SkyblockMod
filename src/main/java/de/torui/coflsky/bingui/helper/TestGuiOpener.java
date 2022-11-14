23ad24188f0c98b8315d85686b2124956716a701
ForBai <thelars42@gmail.com>
2022-11-14 17:43:54 +0100

    fixes

--- src/main/java/de/torui/coflsky/bingui/helper/TestGuiOpener.java
+++ src/main/java/de/torui/coflsky/bingui/helper/TestGuiOpener.java
@@ -11,7 +11,7 @@ public class TestGuiOpener {
     public void openChestEvent(net.minecraftforge.event.entity.player.PlayerInteractEvent event) {
         //ignore this, this will be removed when the gui is finished
         if (event.action.equals(PlayerInteractEvent.Action.RIGHT_CLICK_AIR)) {
-            /*
+/*
             IChatComponent message = new ChatComponentText("FLIP: §6Bat Person Helmet §2500,000 -> 750,000 (+242,500 §448%§2) §7 Med: §b750,000§7 Vol: §b20.7§r§7 sellers ah aüsoijfaoßsijfá0isjf´0aisjfáoijsfá0isjf´0aijsfá9ijf");
             String[] lore = {
                     "This is a Test lore",
@@ -52,7 +52,9 @@ public class TestGuiOpener {
             };
             //Minecraft.getMinecraft().displayGuiScreen(new BinGuiNew(message, lore,"0", "minecraft:apple", null));
             BinGuiManager.openNewFlipGui(message, lore, "0", "leather_leggings");
-             */
+
+ */
+
         }
     }
 }
