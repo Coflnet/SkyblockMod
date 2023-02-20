c01de338888ac8b3c69f67100a51045167bbcd6b
MrFast <78495381+MrFast-js@users.noreply.github.com>
2023-02-19 23:10:23 -0500

    Update CoflSkyCommand.java

--- src/main/java/de/torui/coflsky/CoflSkyCommand.java
+++ src/main/java/de/torui/coflsky/CoflSkyCommand.java
@@ -190,7 +190,6 @@ public class CoflSkyCommand extends CommandBase {
 				}
 			} else {
 				Minecraft.getMinecraft().displayGuiScreen(new CoflGui(true));
-				Minecraft.getMinecraft().setIngameNotInFocus();
 			}
 		}).start();		
 	}
