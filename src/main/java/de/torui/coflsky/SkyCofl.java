0fb523f78c5b460062c324fd81b5e3915f779e6f
Äkwav <ekwav@coflnet.com>
2025-08-08 16:27:01 +0200

    bump mod version

--- src/main/java/de/torui/coflsky/SkyCofl.java
+++ src/main/java/de/torui/coflsky/SkyCofl.java
@@ -29,7 +29,7 @@ import net.minecraftforge.fml.relauncher.Side;
 @Mod(modid = SkyCofl.MODID, version = SkyCofl.VERSION)
 public class SkyCofl {
     public static final String MODID = "SkyCofl";
-    public static final String VERSION = "1.7.0";
+    public static final String VERSION = "1.7.1";
 
     public static KeyBinding[] keyBindings;
 
