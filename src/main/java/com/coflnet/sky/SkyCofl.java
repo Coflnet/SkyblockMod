9cb69929b32572a0e46144c27329af1560e523df
Äkwav <ekwav@coflnet.com>
2026-01-16 23:44:35 +0100

    update to 1.8.4

--- src/main/java/com/coflnet/sky/SkyCofl.java
+++ src/main/java/com/coflnet/sky/SkyCofl.java
@@ -30,7 +30,7 @@ import net.minecraftforge.fml.relauncher.Side;
 public class SkyCofl {
     // this is used as the forge id and has to be lowercase
     public static final String MODID = "skycofl";
-    public static final String VERSION = "1.8.0";
+    public static final String VERSION = "1.8.4";
 
     public static KeyBinding[] keyBindings;
 
